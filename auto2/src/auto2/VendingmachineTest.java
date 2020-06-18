8d0dd935258369df32d8a3439380ffea444b94c2
余榮泰 <aa270239@gmail.com>
2020-06-18 19:04:58 +0800

    精簡ven.test
    精簡ven.test

--- auto2/src/auto2/VendingmachineTest.java
+++ auto2/src/auto2/VendingmachineTest.java
@@ -9,7 +9,7 @@ import auto2.Vendingmachine;
 class VendingmachineTest {
 
 	@Test
-	void testPay1() {
+	void testPay() {
 		Vendingmachine ven = new Vendingmachine();
 		assertEquals(10, ven.pay(20, 0));
 		assertEquals(0, ven.pay(10, 0));
@@ -20,130 +20,49 @@ class VendingmachineTest {
 	
 	
 	@Test
-	void testEnoughMoney1() {
+	void testEnoughMoney() {
 		Vendingmachine ven = new Vendingmachine();
-		assertEquals(true, ven.enoughMoney(15, 0));
-	}
 
-	@Test
-	void testEnoughMoney2() {
-		Vendingmachine ven = new Vendingmachine();
+		assertEquals(true, ven.enoughMoney(15, 0));
 		assertEquals(false, ven.enoughMoney(5, 0));
-	}
-	
-	@Test
-	void testEnoughMoney3() {
-		Vendingmachine ven = new Vendingmachine();
+
 		assertEquals(true, ven.enoughMoney(20, 1));
-	}
-	
-	@Test
-	void testEnoughMoney4() {
-		Vendingmachine ven = new Vendingmachine();
 		assertEquals(false, ven.enoughMoney(5, 1));
-	}
-	
-	
-	@Test
-	void testEnoughMoney5() {
-		Vendingmachine ven = new Vendingmachine();
+
 		assertEquals(true, ven.enoughMoney(20, 2));
-	}
-	
-	@Test
-	void testEnoughMoney6() {
-		Vendingmachine ven = new Vendingmachine();
 		assertEquals(false, ven.enoughMoney(10, 2));
-	}
-	
-	@Test
-	void testEnoughMoney7() {
-		Vendingmachine ven = new Vendingmachine();
+
 		assertEquals(true, ven.enoughMoney(20, 3));
-	}
-	
-	@Test
-	void testEnoughMoney8() {
-		Vendingmachine ven = new Vendingmachine();
 		assertEquals(false, ven.enoughMoney(15, 3));
-	}
-	
-	@Test
-	void testEnoughMoney9() {
-		Vendingmachine ven = new Vendingmachine();
+
 		assertEquals(true, ven.enoughMoney(25, 4));
-	}
-	
-	@Test
-	void testEnoughMoney10() {
-		Vendingmachine ven = new Vendingmachine();
 		assertEquals(false, ven.enoughMoney(5, 4));
-	}
-	
-	@Test
-	void testEnoughMoney11() {
-		Vendingmachine ven = new Vendingmachine();
+
 		assertEquals(true, ven.enoughMoney(30, 5));
-	}
-	
-	@Test
-	void testEnoughMoney12() {
-		Vendingmachine ven = new Vendingmachine();
 		assertEquals(false, ven.enoughMoney(15, 5));
 	}
+
 	
+	s\
 	@Test
-	void testEnoughQuantity1() {
+	void testEnoughQuantity() {
 		Vendingmachine ven = new Vendingmachine();
 		assertEquals(true, ven.enoughQuantity(0));
+		assertEquals(true, ven.enoughQuantity(1));
+		assertEquals(true, ven.enoughQuantity(2));
+		assertEquals(true, ven.enoughQuantity(3));
+		assertEquals(true, ven.enoughQuantity(4));
+		assertEquals(true, ven.enoughQuantity(5));
 	}
 
 	@Test
-	void testEnoughQuantity2() {
-		Vendingmachine ven = new Vendingmachine();
-		assertEquals(true, ven.enoughQuantity(1));
-	}
-	
-	@Test
-	void testEnoughQuantity3() {
-		Vendingmachine ven = new Vendingmachine();
-		assertEquals(true, ven.enoughQuantity(2));
-	}
-	
-	@Test
-	void testEnoughQuantity4() {
-		Vendingmachine ven = new Vendingmachine();
-		assertEquals(true, ven.enoughQuantity(3));
-	}
-	
-	@Test
-	void testEnoughQuantity5() {
-		Vendingmachine ven = new Vendingmachine();
-		assertEquals(true, ven.enoughQuantity(4));
-	}
-	
-	@Test
-	void testEnoughQuantity6() {
-		Vendingmachine ven = new Vendingmachine();
-		assertEquals(true, ven.enoughQuantity(5));
-	}
-	
-	@Test
-	void testCharge1() {
+	void testCharge() {
 		Vendingmachine ven = new Vendingmachine();
 		assertEquals(15, ven.charge(5, 20));
-	}
-	
-	@Test
-	void testCharge2() {
-		Vendingmachine ven = new Vendingmachine();
 		assertEquals(10, ven.charge(15, 25));
-	}
-	
-	@Test
-	void testCharge3() {
-		Vendingmachine ven = new Vendingmachine();
 		assertEquals(20, ven.charge(30, 50));
 	}
+	
+	
 
 }
