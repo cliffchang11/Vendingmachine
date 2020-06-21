b2ec8a19a4ac52cf12804b1ffb4d71ba40495757
余榮泰 <aa270239@gmail.com>
2020-06-22 05:01:17 +0800

    add gradle
    add gradle

---
+++ auto2/src/main/java/ven/Drink.java
@@ -0,0 +1,32 @@
+package main.java.ven;
+
+public class Drink {
+    private int num;
+    private int price;
+    private int quantity;
+    
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public Drink(int num,int price, int quantity){
+        this.num = num;
+        this.price = price ;
+        this.quantity = quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+
+    
+
+}
